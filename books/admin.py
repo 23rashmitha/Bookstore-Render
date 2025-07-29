ab0e9188c9c98e77c379828a41e23c4a960ff876
23rashmitha <rashmithakeshireddy@gmail.com>
2025-07-29 09:52:15 +0530

    all changes

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TeluguBook, HindiBook, EnglishBook, FrenchBook, KannadaBook
+from .models import TeluguBook, HindiBook, EnglishBook, FrenchBook, KannadaBook,MechanicalBook, CivilBook, CseBook, EceBook, EeeBook, ChemicalBook
 
 # Register your models here.
 admin.site.register(TeluguBook)
@@ -7,4 +7,10 @@ admin.site.register(HindiBook)
 admin.site.register(EnglishBook)
 admin.site.register(FrenchBook)
 admin.site.register(KannadaBook)
+admin.site.register(MechanicalBook)
+admin.site.register(CivilBook)
+admin.site.register(CseBook)
+admin.site.register(EceBook)
+admin.site.register(EeeBook)
+admin.site.register(ChemicalBook)
 
